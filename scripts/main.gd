82c8187dbfb0a1016c1734c9aa1ac4a7c2f56e41
Dunocat639YT <francescpasto@gmail.com>
2025-08-05 15:03:40 +0200

    Initial commit

---
+++ scripts/main.gd
@@ -0,0 +1,57 @@
+extends Node
+
+const DINO_START_POS := Vector2i(150, 485)
+const CAM_START_POS := Vector2i(576, 324)
+var score : int
+const SCORE_MODIFIER : int = 10
+var speed : float
+const START_SPEED : float = 3.0
+const MAX_SPEED : int = 15
+var screen_size : Vector2i
+var game_running : bool
+
+
+func _ready() -> void:
+	screen_size = get_window().size
+	new_game()
+	
+
+func new_game():
+	# Reset variables
+	score = 0
+	show_score()
+	game_running = false
+	
+	# Reset the nodes
+	$Dino.position = DINO_START_POS
+	$Dino.velocity = Vector2i(0, 0)
+	$Camera2D.position = CAM_START_POS
+	$Ground.position = Vector2i(0, 0)
+	
+	# Reset HUD
+	$HUD.get_node("StartLabel").show()
+	
+	
+func _process(delta: float) -> void:
+	if game_running:
+		speed = START_SPEED
+			
+		# Move dino and camera
+		$Dino.position.x += speed
+		$Camera2D.position.x += speed
+		
+		#Update score
+		score += speed
+		show_score()
+		
+		# Update ground position
+		if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
+			$Ground.position.x += screen_size.x
+	else:
+		if Input.is_action_pressed("ui_accept"):
+			game_running = true
+			$HUD.get_node("StartLabel").hide()
+	
+	
+func show_score():
+	$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score / SCORE_MODIFIER)
